0cde9f042f7cc4c1b3c15f8a21ebad94e835e159
Mahamudul hasan <mahamudulhasan8960@gmail.com>
2024-10-11 15:41:23 +0600

    Added MVC to the Application

---
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Webgentle";
+        }
+    }
+}
